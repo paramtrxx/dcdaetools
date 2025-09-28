fa99a2ea021da740f50a5bdbd3b35809ed68c83c
paramtrxx <paramtrxx@gmail.com>
2025-09-28 23:48:08 +0300

    добавил квестовые предметы

--- src/main/java/dcdae/dcdaetools/item/ModItemGroups.java
+++ src/main/java/dcdae/dcdaetools/item/ModItemGroups.java
@@ -16,13 +16,59 @@ public class ModItemGroups {
             Identifier.of(DcdaeTools.MOD_ID, "dcdae_group"),
             FabricItemGroup.builder()
                     .displayName(Text.translatable("itemGroup.dcdaetools.dcdae_group"))
-                    .icon(() -> new ItemStack(ModItems.RUBY))
+                    .icon(() -> new ItemStack(ModItems.CHOCOLATE))
                     .entries((context, entries) -> {
                         entries.add(ModBlocks.CHOCOLATE_BLOCK);
                         entries.add(ModItems.CHOCOLATE);
                         entries.add(ModItems.CHOCOLATE_BANANA);
-                        entries.add(ModItems.RUBY);
                         entries.add(ModBlocks.RUBY_BLOCK);
+                    })
+                    .build()
+    );
+
+
+    public static final ItemGroup QUEST_GROUP = Registry.register(
+            Registries.ITEM_GROUP,
+            Identifier.of(DcdaeTools.MOD_ID, "quest_group"),
+            FabricItemGroup.builder()
+                    .displayName(Text.translatable("itemGroup.dcdaetools.quest_group"))
+                    .icon(() -> new ItemStack(ModItems.DEEP_SEE_CRYSTAL))
+                    .entries((context, entries) -> {
+                        entries.add(ModItems.AETHER_ESSENCE);
+                        entries.add(ModItems.ANCIENT_KING_SCEPTER);
+                        entries.add(ModItems.BLACK_PEARL);
+                        entries.add(ModItems.CORRUPTED_GRAIL);
+                        entries.add(ModItems.CORRUPTION_ORB);
+                        entries.add(ModItems.CRYSTAL_PETAL);
+                        entries.add(ModItems.CURSED_CROWN);
+                        entries.add(ModItems.DEEP_SEE_CRYSTAL);
+                        entries.add(ModItems.DESERT_ROSE);
+                        entries.add(ModItems.EVIL_CODEX);
+                        entries.add(ModItems.FROZEN_GRAIL);
+                        entries.add(ModItems.FROZEN_SKULL);
+                        entries.add(ModItems.GOLDEN_LANTERN);
+                        entries.add(ModItems.MAGNIFICENT_CLAM_SHELL);
+                        entries.add(ModItems.MOLTEN_TABLET);
+                        entries.add(ModItems.MOSSY_TABLET);
+                        entries.add(ModItems.PHARAOHS_MASK);
+                        entries.add(ModItems.RED_URN);
+                        entries.add(ModItems.ROTTEN_HEART);
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.SACK_OF_MAGIC_DUST);
+                        entries.add(ModItems.SHACKLES);
+                        entries.add(ModItems.SILVER_FEATHER);
+                        entries.add(ModItems.SKELETAL_HAND);
+                        entries.add(ModItems.STEEL_HEART);
+                        entries.add(ModItems.STONE_IDOL);
+                        entries.add(ModItems.SUPERCOOLED_CORE);
+                        entries.add(ModItems.TOXIC_VIAL);
+                        entries.add(ModItems.TRIBAL_TOTEM);
+                        entries.add(ModItems.UNDEAD_ESSENCE);
+                        entries.add(ModItems.UNHOLY_CANDLE);
+                        entries.add(ModItems.WARDEN_EARS);
+                        entries.add(ModItems.WICKED_TENTACLE);
+                        entries.add(ModItems.WILDFIRE_ESSENCE);
+                        entries.add(ModItems.WITHER_RIBCAGE);
                     })
                     .build()
     );
--- src/main/java/dcdae/dcdaetools/item/ModItems.java
+++ src/main/java/dcdae/dcdaetools/item/ModItems.java
@@ -11,16 +11,50 @@ import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item RUBY = registerItem("ruby", Item::new, new Item.Settings()
-            .maxCount(64));
+    public static final Item RUBY = registerItem("ruby", Item::new, new Item.Settings());
 
     public static final Item CHOCOLATE = registerItem("chocolate", Item::new, new Item.Settings()
-            .maxCount(64)
             .food(ModFoodComponents.CHOCOLATE));
 
     public static final Item CHOCOLATE_BANANA = registerItem("chocolate_banana", Item::new,
             new Item.Settings()
                     .maxCount(64).food(ModFoodComponents.CHOCOLATE_BANANA, ModFoodComponents.CHOCOLATE_BANANA_EFFECT));
+
+    public static final Item AETHER_ESSENCE = registerItem("aether_essence", Item::new, new Item.Settings().maxCount(1));
+    public static final Item ANCIENT_KING_SCEPTER = registerItem("ancient_king_scepter", Item::new, new Item.Settings().maxCount(1));
+    public static final Item BLACK_PEARL = registerItem("black_pearl", Item::new, new Item.Settings().maxCount(16));
+    public static final Item CORRUPTED_GRAIL = registerItem("corrupted_grail", Item::new, new Item.Settings().maxCount(1));
+    public static final Item CORRUPTION_ORB = registerItem("corruption_orb", Item::new, new Item.Settings().maxCount(16));
+    public static final Item CRYSTAL_PETAL = registerItem("crystal_petal", Item::new, new Item.Settings());
+    public static final Item CURSED_CROWN = registerItem("cursed_crown", Item::new, new Item.Settings().maxCount(1));
+    public static final Item DEEP_SEE_CRYSTAL = registerItem("deep_see_crystal", Item::new, new Item.Settings());
+    public static final Item DESERT_ROSE = registerItem("desert_rose", Item::new, new Item.Settings());
+    public static final Item EVIL_CODEX = registerItem("evil_codex", Item::new, new Item.Settings().maxCount(1));
+    public static final Item FROZEN_GRAIL = registerItem("frozen_grail", Item::new, new Item.Settings().maxCount(1));
+    public static final Item FROZEN_SKULL = registerItem("frozen_skull", Item::new, new Item.Settings());
+    public static final Item GOLDEN_LANTERN = registerItem("golden_lantern", Item::new, new Item.Settings().maxCount(1));
+    public static final Item MAGNIFICENT_CLAM_SHELL = registerItem("magnificent_clam_shell", Item::new, new Item.Settings());
+    public static final Item MOLTEN_TABLET = registerItem("molten_tablet", Item::new, new Item.Settings());
+    public static final Item MOSSY_TABLET = registerItem("mossy_tablet", Item::new, new Item.Settings());
+    public static final Item PHARAOHS_MASK = registerItem("pharaohs_mask", Item::new, new Item.Settings().maxCount(1));
+    public static final Item RED_URN = registerItem("red_urn", Item::new, new Item.Settings().maxCount(1));
+    public static final Item ROTTEN_HEART = registerItem("rotten_heart", Item::new, new Item.Settings());
+    public static final Item SACK_OF_MAGIC_DUST = registerItem("sack_of_magic_dust", Item::new, new Item.Settings().maxCount(16));
+    public static final Item SHACKLES = registerItem("shackles", Item::new, new Item.Settings().maxCount(1));
+    public static final Item SILVER_FEATHER = registerItem("silver_feather", Item::new, new Item.Settings());
+    public static final Item SKELETAL_HAND = registerItem("skeletal_hand", Item::new, new Item.Settings());
+    public static final Item STEEL_HEART = registerItem("steel_heart", Item::new, new Item.Settings());
+    public static final Item STONE_IDOL = registerItem("stone_idol", Item::new, new Item.Settings().maxCount(1));
+    public static final Item SUPERCOOLED_CORE = registerItem("supercooled_core", Item::new, new Item.Settings());
+    public static final Item TOXIC_VIAL = registerItem("toxic_vial", Item::new, new Item.Settings().maxCount(1));
+    public static final Item TRIBAL_TOTEM = registerItem("tribal_totem", Item::new, new Item.Settings().maxCount(1));
+    public static final Item UNDEAD_ESSENCE = registerItem("undead_essence", Item::new, new Item.Settings());
+    public static final Item UNHOLY_CANDLE = registerItem("unholy_candle", Item::new, new Item.Settings());
+    public static final Item WARDEN_EARS = registerItem("warden_ears", Item::new, new Item.Settings());
+    public static final Item WICKED_TENTACLE = registerItem("wicked_tentacle", Item::new, new Item.Settings());
+    public static final Item WILDFIRE_ESSENCE = registerItem("wildfire_essence", Item::new, new Item.Settings().maxCount(1));
+    public static final Item WITHER_RIBCAGE = registerItem("wither_ribcage", Item::new, new Item.Settings());
+
 
 
     public static Item registerItem(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
